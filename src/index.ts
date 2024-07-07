fd0c816319d2a8354c067c5a6932cc04bc4d2730
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-07-07 18:27:50 -0400

    feat: add default index export

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// * File format classes and exports
+export { default as Certificate } from '@/certificate';
+export * from '@/certificate';
+
+export { default as CIA } from '@/cia';
+export * from '@/cia';
+
+export { default as SMDH } from '@/smdh';
+export * from '@/smdh';
+
+export { default as Ticket } from '@/ticket';
+export * from '@/ticket';
+
+export { default as TMD } from '@/tmd';
+export * from '@/tmd';
+
+// * Utility classes and exports
+export { default as Stream } from '@/stream';
+export { default as FileStream } from '@/file-stream';
+export * from '@/signatures';
