0cff403e4b772e1fb97711452eddcc4b5f870065
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-07-08 16:11:13 -0400

    chore: update class exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 // * File format classes and exports
+export { default as BYAML } from '@/byaml';
+export { default as BYML } from '@/byaml'; // * Alias of BYAML
+export * from '@/byaml';
+
 export { default as Certificate } from '@/certificate';
 export * from '@/certificate';
 
 export { default as CIA } from '@/cia';
 export * from '@/cia';
+
+export { default as MSBT } from '@/msbt';
+export * from '@/msbt';
 
 export { default as SMDH } from '@/smdh';
 export * from '@/smdh';
