dcc3a6a43c9c1b0cf5f0a74ae5d8e6d349b13620
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-09-21 01:03:33 -0400

    feat(mii): add TSDoc comments to FFLiMiiDataCore fields

--- src/mii/ffl-imii-data-core.ts
+++ src/mii/ffl-imii-data-core.ts
@@ -20,10 +20,10 @@ const field0x03Parser = new Parser()
 const field0x0CParser = new Parser()
 	.endianness('big')
 	.bit28('creationDate')
-	.bit1('alwaysSet')
-	.bit1('isTemporary')
+	.bit1('isValid')
+	.bit1('isNonUserMii')
 	.bit1('isDSi')
-	.bit1('isSpecialInverted');
+	.bit1('isNormalMii');
 
 const field0x18Parser = new Parser()
 	.bit1('sex')
@@ -146,12 +146,7 @@ const coreParser = new Parser()
 	.nest('field0x46', { type: field0x46Parser });
 
 const createIDParser = new Parser()
-	.endianness('big')
-	.bit28('creationDate')
-	.bit1('alwaysSet')
-	.bit1('isTemporary')
-	.bit1('isDSi')
-	.bit1('isSpecialInverted')
+	.nest('field0x0C', { type: field0x0CParser })
 	.buffer('mac', { length: 6 });
 
 type DecodedFFLiMiiDataCore = {
@@ -175,10 +170,10 @@ type DecodedFFLiMiiDataCore = {
 	field0x04: Buffer;
 	field0x0C: {
 		creationDate: number;
-		alwaysSet: number;
-		isTemporary: number;
+		isValid: number;
+		isNonUserMii: number;
 		isDSi: number;
-		isSpecialInverted: number;
+		isNormalMii: number;
 	};
 	field0x10: Buffer;
 	field0x18: {
@@ -311,49 +306,90 @@ export default class FFLiMiiDataCore {
 	private stream: FileStream;
 	private decoded!: DecodedFFLiMiiDataCore;
 
+	/**
+	 * The Mii data version. Always 3, unless the Mii was made
+	 * with the Wii U GamePad camera, in which case 0.
+	 */
 	@mapToDecoded('field0x00')
 	public miiVersion: number;
 
+	/**
+	 * If set, the Mii can be copied.
+	 */
 	@mapToDecoded('field0x01.allowCopying', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
 	public copyable: boolean;
 
+	/**
+	 * If set, this means the Mii contains profanity in either
+	 * the Mii name or creator name. If set, both the Mii name
+	 * and creator name are replaced with "???".
+	 */
 	@mapToDecoded('field0x01.hasProfanity', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
 	public ngWord: boolean;
 
+	/**
+	 * If set, this locks the Mii to a specific region.
+	 */
 	@mapToDecoded('field0x01.regionLock')
 	public regionMove: number;
 
+	/**
+	 * Denotes the font type that should be used for strings.
+	 */
 	@mapToDecoded('field0x01.characterSet')
 	public fontRegion: number;
 
+	/**
+	 * The room ID the Mii is in when using Mii Maker.
+	 */
 	@mapToDecoded('field0x02.pageIndex')
 	public roomIndex: number;
 
+	/**
+	 * The slot ID the Mii is in when using Mii Maker.
+	 */
 	@mapToDecoded('field0x02.slotIndex')
 	public positionInRoom: number;
 
+	/**
+	 * Unknown.
+	 */
 	@mapToDecoded('field0x03.unknown')
 	public authorType: number;
 
+	/**
+	 * Denotes the platform the Mii was created on.
+	 */
 	@mapToDecoded('field0x03.deviceOrigin')
 	public birthPlatform: number;
 
+	/**
+	 * Identifies the owner of the Mii, editing restrictions and blue pants.
+	 * In older Mii formats, this was tied to the console MAC. This is no
+	 * longer the case.
+	 */
 	@mapToDecoded('field0x04')
 	public authorID: Buffer;
 
+	/**
+	 * In the original implementation, this contains additional flags and metadata.
+	 * In more recent implementations, this is random data.
+	 */
 	public get createID(): Buffer {
 		return createIDParser.encode({
-			creationDate: this.creationDate,
-			alwaysSet: this.alwaysSet,
-			isTemporary: this.isTemporary,
-			isDSi: this.isDSi,
-			isSpecialInverted: this.isSpecialInverted,
+			field0x0C: {
+				creationDate: this.creationDate,
+				isValid: this.isValid,
+				isNonUserMii: this.isNonUserMii,
+				isDSi: this.isDSi,
+				isNormalMii: this.isNormalMii,
+			},
 			mac: this.creatorMAC
 		});
 	}
@@ -361,206 +397,394 @@ export default class FFLiMiiDataCore {
 	public set createID(createID: Buffer) {
 		const decoded = createIDParser.parse(createID);
 
-		this.creationDate = decoded.creationDate;
-		this.alwaysSet = decoded.alwaysSet;
-		this.isTemporary = decoded.isTemporary;
-		this.isDSi = decoded.isDSi;
-		this.isSpecialInverted = decoded.isSpecialInverted;
+		this.creationDate = decoded.field0x0C.creationDate;
+		this.isValid = decoded.field0x0C.isValid;
+		this.isNonUserMii = decoded.field0x0C.isNonUserMii;
+		this.isDSi = decoded.field0x0C.isDSi;
+		this.isNormalMii = decoded.field0x0C.isNormalMii;
 		this.creatorMAC = decoded.mac;
 	}
 
+	/**
+	 * The date the Mii was created, in seconds since 01/01/2010 00:00:00.
+	 * This is part of the "createID" in the original implementation, and
+	 * should not be relied on since more recent implementations of "createID"
+	 * set this to random data.
+	 */
 	@mapToDecoded('field0x0C.creationDate')
 	public creationDate: number;
 
-	@mapToDecoded('field0x0C.alwaysSet', {
+	/**
+	 * If set, the Mii is considered valid. If not, the Mii is considered invalid.
+	 * This is part of the "createID" in the original implementation, and
+	 * should not be relied on since more recent implementations of "createID"
+	 * set this to random data.
+	 */
+	@mapToDecoded('field0x0C.isValid', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
-	public alwaysSet: boolean;
+	public isValid: boolean;
 
-	@mapToDecoded('field0x0C.isTemporary', {
+	/**
+	 * If set, this Mii is either a temporary Mii made by an application (such as an NPC),
+	 * or this is a developer Mii made on a dev unit.
+	 * This is part of the "createID" in the original implementation, and
+	 * should not be relied on since more recent implementations of "createID"
+	 * set this to random data.
+	 */
+	@mapToDecoded('field0x0C.isNonUserMii', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
-	public isTemporary: boolean;
+	public isNonUserMii: boolean;
 
+	/**
+	 * If set, this Mii was created on a DSi.
+	 * This is part of the "createID" in the original implementation, and
+	 * should not be relied on since more recent implementations of "createID"
+	 * set this to random data.
+	 */
 	@mapToDecoded('field0x0C.isDSi', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
 	public isDSi: boolean;
 
-	@mapToDecoded('field0x0C.isSpecialInverted', {
+	/**
+	 * If set, this Mii will NOT have the "special Mii" gold pants.
+	 * This is part of the "createID" in the original implementation, and
+	 * should not be relied on since more recent implementations of "createID"
+	 * set this to random data.
+	 */
+	@mapToDecoded('field0x0C.isNormalMii', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
-	public isSpecialInverted: boolean;
+	public isNormalMii: boolean;
 
+	/**
+	 * The MAC address of the console the Mii was made on.
+	 * This is part of the "createID" in the original implementation, and
+	 * should not be relied on since more recent implementations of "createID"
+	 * set this to random data.
+	 */
 	@mapToDecoded('field0x10')
 	public creatorMAC: Buffer;
 
+	/**
+	 * Denotes the Miis gender.
+	 */
 	@mapToDecoded('field0x18.sex')
 	public gender: number;
 
+	/**
+	 * Denotes the Miis birth month.
+	 */
 	@mapToDecoded('field0x18.birthMonth')
 	public birthMonth: number;
 
+	/**
+	 * Denotes the Miis birth day.
+	 */
 	@mapToDecoded('field0x18.birthDay')
 	public birthDay: number;
 
+	/**
+	 * Denotes the Miis shirt color.
+	 */
 	@mapToDecoded('field0x18.favoriteColor')
 	public favoriteColor: number;
 
+	/**
+	 * If set, this Mii is considered a users "favorite".
+	 */
 	@mapToDecoded('field0x18.isFavorite', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
 	public favorite: boolean;
 
+	/**
+	 * Mii name. UTF-16 encoded, up to 10 characters with "0000" padding
+	 */
 	@mapToDecoded('field0x1A')
 	public name: string;
 
+	/**
+	 * Height of the Mii
+	 */
 	@mapToDecoded('field0x2E.height')
 	public height: number;
 
+	/**
+	 * Thickness of the Mii
+	 */
 	@mapToDecoded('field0x2E.width')
 	public build: number;
 
+	/**
+	 * If set, the Mii cannot be shared via StreetPass. If the Mii has "isNormalMii",
+	 * it MUST have this flag set. Special Miis are not permitted to be shared.
+	 */
 	@mapToDecoded('field0x30.disableSharing', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
 	public localOnly: boolean;
 
+	/**
+	 * Mii head shape ID.
+	 */
 	@mapToDecoded('field0x30.faceShape')
 	public faceType: number;
 
+	/**
+	 * Mii skin color ID.
+	 */
 	@mapToDecoded('field0x30.skinColor')
 	public faceColor: number;
 
+	/**
+	 * Mii wrinkles type ID.
+	 */
 	@mapToDecoded('field0x31.wrinkles')
 	public faceTex: number;
 
+	/**
+	 * Mii makeup type ID.
+	 */
 	@mapToDecoded('field0x31.makeup')
 	public faceMake: number;
 
+	/**
+	 * Mii hair type ID.
+	 */
 	@mapToDecoded('field0x32')
 	public hairType: number;
 
+	/**
+	 * Mii hair color ID.
+	 */
 	@mapToDecoded('field0x33.hairColor')
 	public hairColor: number;
 
+	/**
+	 * If set, mirror the Miis hair.
+	 */
 	@mapToDecoded('field0x33.flipHair', {
 		get: (value: unknown) => !!value,
 		set: (value: unknown) => value ? 1 : 0
 	})
 	public hairFlip: boolean;
 
+	/**
+	 * Mii eye type ID.
+	 */
 	@mapToDecoded('field0x34.eyeStyle')
 	public eyeType: number;
 
+	/**
+	 * Mii eye color ID.
+	 */
 	@mapToDecoded('field0x34.eyeColor')
 	public eyeColor: number;
 
+	/**
+	 * Mii eye scale.
+	 */
 	@mapToDecoded('field0x34.eyeScale')
 	public eyeScale: number;
 
+	/**
+	 * Mii eye height.
+	 */
 	@mapToDecoded('field0x34.eyeYScale')
 	public eyeAspect: number;
 
+	/**
+	 * Mii eye rotation amount.
+	 */
 	@mapToDecoded('field0x34.eyeRotation')
 	public eyeRotate: number;
 
+	/**
+	 * Mii eye X position.
+	 */
 	@mapToDecoded('field0x34.eyeXSpacing')
 	public eyeX: number;
 
+	/**
+	 * Mii eye Y position.
+	 */
 	@mapToDecoded('field0x34.eyeYPosition')
 	public eyeY: number;
 
+	/**
+	 * Mii eyebrow type ID.
+	 */
 	@mapToDecoded('field0x38.eyebrowStyle')
 	public eyebrowType: number;
 
+	/**
+	 * Mii eyebrow color ID.
+	 */
 	@mapToDecoded('field0x38.eyebrowColor')
 	public eyebrowColor: number;
 
+	/**
+	 * Mii eyebrow scale.
+	 */
 	@mapToDecoded('field0x38.eyebrowScale')
 	public eyebrowScale: number;
 
+	/**
+	 * Mii eyebrow height.
+	 */
 	@mapToDecoded('field0x38.eyebrowYScale')
 	public eyebrowAspect: number;
 
+	/**
+	 * Mii eyebrow rotation amount.
+	 */
 	@mapToDecoded('field0x38.eyebrowRotation')
 	public eyebrowRotate: number;
 
+	/**
+	 * Mii eyebrow X position.
+	 */
 	@mapToDecoded('field0x38.eyebrowXSpacing')
 	public eyebrowX: number;
 
+	/**
+	 * Mii eyebrow Y position.
+	 */
 	@mapToDecoded('field0x38.eyebrowYPosition')
 	public eyebrowY: number;
 
+	/**
+	 * Mii nose type ID.
+	 */
 	@mapToDecoded('field0x3C.noseStyle')
 	public noseType: number;
 
+	/**
+	 * Mii nose scale.
+	 */
 	@mapToDecoded('field0x3C.noseScale')
 	public noseScale: number;
 
+	/**
+	 * Mii nose Y position.
+	 */
 	@mapToDecoded('field0x3C.noseYPosition')
 	public noseY: number;
 
+	/**
+	 * Mii mouth type ID.
+	 */
 	@mapToDecoded('field0x3E.mouthStyle')
 	public mouthType: number;
 
+	/**
+	 * Mii mouth color ID.
+	 */
 	@mapToDecoded('field0x3E.mouthColor')
 	public mouthColor: number;
 
+	/**
+	 * Mii mouth scale.
+	 */
 	@mapToDecoded('field0x3E.mouthScale')
 	public mouthScale: number;
 
+	/**
+	 * Mii mouth height.
+	 */
 	@mapToDecoded('field0x3E.mouthYScale')
 	public mouthAspect: number;
 
+	/**
+	 * Mii mouth Y position.
+	 */
 	@mapToDecoded('field0x40.mouthYPosition')
 	public mouthY: number;
 
+	/**
+	 * Mii mustache type ID.
+	 */
 	@mapToDecoded('field0x40.mustacheStyle')
 	public mustacheType: number;
 
+	/**
+	 * Mii beard type ID.
+	 */
 	@mapToDecoded('field0x42.beardStyle')
 	public beardType: number;
 
+	/**
+	 * Mii facial hair color ID. Affects both beard and mustache.
+	 */
 	@mapToDecoded('field0x42.beardColor')
 	public beardColor: number;
 
+	/**
+	 * Mii mustache scale. Despite the name, this does not affect the beard in any way.
+	 */
 	@mapToDecoded('field0x42.mustacheScale')
 	public beardScale: number;
 
+	/**
+	 * Mii mustache Y position. Despite the name, this does not affect the beard in any way.
+	 */
 	@mapToDecoded('field0x42.mustacheYPosition')
 	public beardY: number;
 
+	/**
+	 * Mii glasses type ID.
+	 */
 	@mapToDecoded('field0x44.glassesStyle')
 	public glassType: number;
 
+	/**
+	 * Mii glasses color ID.
+	 */
 	@mapToDecoded('field0x44.glassesColor')
 	public glassColor: number;
 
+	/**
+	 * Mii glasses scale.
+	 */
 	@mapToDecoded('field0x44.glassesScale')
 	public glassScale: number;
 
+	/**
+	 * Mii glasses Y position.
+	 */
 	@mapToDecoded('field0x44.glassesYPosition')
 	public glassY: number;
 
-	@mapToDecoded('field0x46.enableMole', {
-		get: (value: unknown) => !!value,
-		set: (value: unknown) => value ? 1 : 0
-	})
-	public moleType: boolean;
+	/**
+	 * If set, the Miis mole is visible.
+	 */
+	@mapToDecoded('field0x46.enableMole')
+	public moleType: number;
 
+	/**
+	 * Mii mole scale.
+	 */
 	@mapToDecoded('field0x46.moleScale')
 	public moleScale: number;
 
+	/**
+	 * Mii mole X position.
+	 */
 	@mapToDecoded('field0x46.moleXPosition')
 	public moleX: number;
 
+	/**
+	 * Mii mole Y position.
+	 */
 	@mapToDecoded('field0x46.moleYPosition')
 	public moleY: number;
 
