8e2c54210a030930a54c7e0982fa85b69c87db28
Jonathan Barrow <jonbarrow1998@gmail.com>
2026-05-27 16:33:40 -0400

    chore: update BYAML import in tests

--- test/byaml.test.ts
+++ test/byaml.test.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { describe, it, expect } from 'vitest';
-import { BYAML } from '@/byaml';
+import BYAML from '@/byaml';
 
 const SAMPLES_DIR = path.join(__dirname, './samples/byaml');
 
